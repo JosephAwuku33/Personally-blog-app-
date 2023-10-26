cb954033007ef819348c611e30b94001dfd93e0f
JosephAwuku33 <33jaykay@gmail.com>
2023-10-26 18:23:45 +0000

    simple test

--- src/components/HomeArticle.js
+++ src/components/HomeArticle.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HomeArticle() {
     return (
         <article className="items-center justify-center flex-1 ">
-            <h1 className="text-6xl text-gray-dark text-center break-normal font-bold mx-3 mt-32 justify-center">Minimal blog for creative expressions</h1>
+            <h1 className="text-6xl text-gray-dark text-center break-normal font-bold mx-3 mt-32 justify-center">Minimal blog for creative expressions like YOU</h1>
             <h2 className="text-lg text-gray-light text-center break-normal mx-5 mt-4 font-light">100% customizable and tech news on the go for commercial and personal purposes</h2>
         </article>
     );
