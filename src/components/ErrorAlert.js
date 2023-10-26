24871270bcb5ac1ab3af9fd35346964d4fd39342
JosephAwuku33 <33jaykay@gmail.com>
2023-10-26 19:15:21 +0000

    a dey wan see something

--- src/components/ErrorAlert.js
+++ src/components/ErrorAlert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ErrorAlert({ message }) {
   return (
-    <div role="alert" className="mb-2">
+    <div role="alert" className="mb-3">
         <div className ="border border-red-dark rounded-b bg-light-red px-4 py-2 text-white">
             <p>{message}</p>
         </div>
